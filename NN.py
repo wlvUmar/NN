3a0a9bd20854b6633ae726f80673595f061d68b5
azimovic1 <U.Tolibov@wlv.ac.uk>
2025-05-10 15:01:48 +0500

    adfs

--- NN.py
+++ NN.py
@@ -16,10 +16,12 @@ class MyNN():
         """
         self.layer_sizes = layer_sizes
         self.learning_rate = learning_rate
+        self.momentum = momentum
+        
         self.task = task  
         self.optimizer = optimizer
         self.activation = activation
-        self.momentum = momentum
+        
         self.weights = [np.random.randn(n_in, n_out) for n_in, n_out in zip(layer_sizes[:-1], layer_sizes[1:])]
         self.biases = [np.random.randn(n_out) for n_out in layer_sizes[1:]]
         self.velocity_w = [np.zeros_like(w) for w in self.weights]
@@ -27,6 +29,7 @@ class MyNN():
 
     @staticmethod
     def sigmoid(x):
+        x = np.clip(x, -500, 500) 
         return 1 / (1 + np.exp(-x))
 
     @staticmethod
@@ -58,14 +61,14 @@ class MyNN():
         and going to the next layer. And z score in each iter is the weighted sums of each neuron in 2d vector)
         
         Args:
-        inputs (ndarray): The input data.
+        inputs (ndarray)[[x1,x2]]: The input data.
         
         Returns:
         list: The list of activations for each layer.
         activations: final outputs of the layers(layer neurons)
         z_values: nonactivated outputs
         """
-        activations = [inputs] # arr(1,0)(1,2) * w(2,3)
+        activations = [inputs] # [ndarray([[x1,x2 ]])] 
         z_values = []  # Z values (inputs to the activation functions)
         for i, (w, b) in enumerate(zip(self.weights, self.biases)):
             z = np.dot(activations[-1], w) + b
@@ -103,28 +106,41 @@ class MyNN():
         Perform backpropagation to calculate the gradients and update weights.
         
         Args:
-        inputs (ndarray): The input data.
-        true_output (ndarray): The expected output.
+        inputs (ndarray) [[x1,x2]]: The input data.
+        true_output (ndarray) [[y]]: The expected output.
         
         Returns:
         tuple: Updated weights and biases.
         """
         activations, z_values = self.forward_pass(inputs)
         error = activations[-1] - true_output  # errors in each
-        grad_output = error * self.sigmoid_derivative(activations[-1])
+        if self.task == 'r' and self.activation == 'linear':
+            grad_output = error
+        else:
+            if self.activation == "sigmoid":
+                grad_output = error * self.sigmoid_derivative(z_values[-1])
+            elif self.activation == "relu":
+                grad_output = error * self.relu_derivative(z_values[-1])
+            else:
+                grad_output = error  
+
         grads_w = []
         grads_b = []
 
         # Backpropagate the gradients for each layer
-        for i in reversed(range(len(self.weights))):  # n-1, n-2, ... 3, 2, 1, 0
+        for i in reversed(range(len(self.weights))): 
             grad_w = np.outer(activations[i], grad_output)
             grad_b = grad_output.flatten()
             grads_w.append(grad_w)
             grads_b.append(grad_b)
             
-            # Propagate the gradient backwards to the previous layer
             if i > 0:
-                grad_output = np.dot(grad_output, self.weights[i].T) * self.sigmoid_derivative(activations[i])
+                if self.activation == "sigmoid":
+                    grad_output = np.dot(grad_output, self.weights[i].T) * self.sigmoid_derivative(z_values[i-1])
+                elif self.activation == "relu":
+                    grad_output = np.dot(grad_output, self.weights[i].T) * self.relu_derivative(z_values[i-1])
+                else:
+                    grad_output = np.dot(grad_output, self.weights[i].T)
         grads_w = grads_w[::-1]
         grads_b = grads_b[::-1]
         self.update_weights(grads_w, grads_b)
@@ -132,10 +148,10 @@ class MyNN():
     def train(self, X_train, y_train, epochs=1000):
         for epoch in range(epochs):
             total_loss = 0
-            for x, y in zip(X_train, y_train):
+            for x, y in zip(X_train, y_train): # [(x1,y1), (x2,y2)..]
                 # Reshape the input data and target data
-                x = x.reshape(1, -1)
-                y = y.reshape(-1, 1)
+                x = x.reshape(1, -1) # ndarray(shape=(1,nfeatures)) [[x1,x2,]]
+                y = y.reshape(-1, 1) # ndarray(shape=(1,1)), [[y]]
 
                 # Perform backpropagation
                 self.backpropagate(x, y)
